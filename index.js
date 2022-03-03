6d0888cf69c67b2e845cfbc585dd818b7a8b4deb
KTMichael <kmpg1822@gmail.com>
2022-03-03 15:42:52 -0800

    fixing server heroku issue

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express();
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
 
 app.use(express.static("public"));
 
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
+var server_host = process.env.YOUR_HOST || "0.0.0.0";
+app.listen(server_port, server_host, function () {
+  console.log("Listening on port %d", server_port);
+});
