c41aa2153babc2f0b26f2142047705cf2094084c
KTMichael <kmpg1822@gmail.com>
2022-03-02 19:10:14 -0800

    finished game

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
-let lastHole;
-let timeUp = false;
-let score = 0;
+const finalScoreContainer = document.getElementById("finalScoreContainer");
+const playersFinalScore = document.getElementById("playersFinalScore");
+const closeFinalScore = document.getElementById("closeFinalScore");
+
+let lastHole, time, level, score, timeUp, hole;
+let btn = document.getElementById("btn");
+let mode = document.getElementById("mode");
+
+function start() {
+  mode.textContent = "Easy";
+  finalScoreContainer.style.display = "none";
+  score = 0;
+  scoreBoard.textContent = score;
+  timeUp = false;
+  level = "easy";
+  btn.textContent = "Start The Game!";
+}
 
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -20,8 +34,14 @@ function randomHole(holes) {
 }
 
 function peep() {
-  const time = randomTime(500, 1000);
-  const hole = randomHole(holes);
+  console.log(level);
+  if (level === "easy") {
+    time = randomTime(900, 1000);
+  }
+  if (level === "hard") {
+    time = randomTime(200, 1000);
+  }
+ hole = randomHole(holes);
   hole.classList.add("up");
   setTimeout(() => {
     hole.classList.remove("up");
@@ -29,12 +49,26 @@ function peep() {
   }, time);
 }
 
+function changeButton() {
+  if (timeUp === false) {
+    btn.textContent = "Whack!";
+  }
+}
+
+function showFinalScore() {
+  finalScoreContainer.style.display = "block";
+  playersFinalScore.textContent = score;
+  timeUp = true;
+  closeFinalScore.addEventListener("click", function () {
+    start();
+  });
+}
+
 function startGame() {
-  scoreBoard.textContent = 0;
   timeUp = false;
-  score = 0;
   peep();
-  setTimeout(() => (timeUp = true), 10000);
+  changeButton();
+  setTimeout(() => showFinalScore(), 10000);
 }
 
 function whack(e) {
@@ -45,3 +79,19 @@ function whack(e) {
 }
 
 moles.forEach((mole) => mole.addEventListener("click", whack));
+
+var checkbox = document.querySelector('input[type="checkbox"]');
+
+checkbox.addEventListener("change", function () {
+  if (checkbox.checked) {
+    mode.textContent = "Hard";
+    console.log("Checked");
+    level = "hard";
+  } else {
+    console.log("Not checked");
+    mode.textContent = "Easy";
+    level = "easy";
+  }
+});
+
+start();
